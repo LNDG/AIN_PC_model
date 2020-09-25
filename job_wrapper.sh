501e86b8de7488084c2c26afea50064ffd978162
FabianKamp <fabian.kamp@fu-berlin.de>
2020-09-25 15:31:04 +0200

    job and modelfile fixed

--- job_wrapper.sh
+++ job_wrapper.sh
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-ranges=( "noise_lowcut=5" "noise_lowcut=10" "noise_lowcut=50" "noise_lowcut=100" )
-=======
 ranges=( "noise_highcut=400" "noise_highcut=450" "noise_highcut=490" "noise_highcut=495" )
->>>>>>> ef95ca6c58e7af564d0b541e37beb8e29712681f
 modelfiles=( "colored_noise_gaba.py" )
 
 
 for model in "${modelfiles[@]}"; do
 	for range in "${ranges[@]}"; do
-<<<<<<< HEAD
-		old_range=$(grep "noise_lowcut=" model_config.py)
-=======
 		old_range=$(grep "noise_highcut=" model_config.py)
->>>>>>> ef95ca6c58e7af564d0b541e37beb8e29712681f
 		sed -i "s/$old_range/$range/g" model_config.py
 		echo "#!/bin/bash" > jobfile.sh
 		echo "#SBATCH --job-name AttractorSimulation" >> jobfile.sh
--- model_config.py
+++ model_config.py
@@ -1,16 +1,8 @@
 import numpy as np
 simulate=True
-<<<<<<< HEAD
-nr_timepoints = 60000
-noise_level_range = np.linspace(0.0025, 0.1, 10) # defines the range of noise level
-noise_lowcut=10
-noise_highcut = 300
-mu = {'XL': 1.0, 'XR': 1.0, 'beta': 0.24, 'gamma': 3.75, 'exponent': 1.0, 'alpha': 1.75, 'tau': 100.0, 'NRa': 2.0, 'NRs': 1.0, 'noise_level': 0.0025, 'var_inh': 120.0, 'tau_inh': 50, 'var_inh_infl': 0.0, 'NRa_var_inh': 2.0, 'NRs_var_inh': 1.0, 'var_inh_noise_level': 0.000, 'var_inh_noise_infl': 0.00001, 'noise_color':1}
-=======
 nr_timepoints=60000
 noise_level_range=np.linspace(0.0025, 0.1, 10) # defines the range of noise level
 noise_lowcut=5
 noise_highcut=495
 mu={'XL': 1.0, 'XR': 1.0, 'beta': 0.24, 'gamma': 3.75, 'exponent': 1.0, 'alpha': 1.75, 'tau': 100.0, 'NRa': 2.0, 'NRs': 1.0, 'noise_level': 0.0025, 'var_inh': 120.0, 'tau_inh': 50, 'var_inh_infl': 0.0, 'NRa_var_inh': 2.0, 'NRs_var_inh': 1.0, 'var_inh_noise_level': 0.000, 'var_inh_noise_infl': 0.00001, 'noise_color':1}
->>>>>>> ef95ca6c58e7af564d0b541e37beb8e29712681f
 mu.update({'noise_lowcut': noise_lowcut, 'noise_highcut': noise_highcut})
