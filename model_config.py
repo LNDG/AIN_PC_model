43e9bf5ffb4d5e3dfb64b870f72f08bc9d1bccc5
clear <fabian.kamp@fu-berlin.de>
2020-10-03 19:54:28 +0200

    created models and simulation functions

--- model_config.py
+++ model_config.py
@@ -7,4 +7,4 @@ noise_highcut=300
 noise_color=1
 #mu={'XL': 1.0, 'XR': 1.25, 'beta': 0.24, 'gamma': 3.25, 'exponent': 1.0, 'alpha': 1.75, 'tau': 100.0, 'NRa': 2.0, 'NRs': 1.0, 'noise_level': 0.0025, 'var_inh': 120.0, 'tau_inh': 50, 'var_inh_infl': 0.0, 'NRa_var_inh': 2.0, 'NRs_var_inh': 1.0, 'var_inh_noise_level': 0.000, 'var_inh_noise_infl': 0.00001, 'noise_color':1}
 mu={'XL': 1.0, 'XR': 1.25, 'gamma': 3.25, 'exponent': 1.0, 'alpha': 1.75, 'tau': 100.0, 'NRa': 2.0, 'NRs': 1.0}
-mu.update({'noise_lowcut': noise_lowcut, 'noise_highcut': noise_highcut, 'noise_color': noise_color})
+mu.update({'noise_lowcut': noise_lowcut, 'noise_highcut': noise_highcut, 'noise_nr': noise_color})
